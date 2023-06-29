fc3bb40e21b001b3fa9c73bebd39f45c1838d6ae
Gabriel de O. Pacheco <gabriel_o_pacheco@estudante.sesisenai.org.br>
2023-06-28 21:20:08 -0300

    criando formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
 
+import FormComponent from './assents/components/FormComponent/FormComponent'
+
 function App() {
   return (
     <>
       <p>App is Render</p>
+      <FormComponent/>
     </>
   )
 }
---
+++ src/assents/components/FormComponent/FormComponent.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react"
+// formulario reativo
+const FormComponent = () => {
+const [data, setData] = useState({
+    nome:'',
+    email:'',
+    senha:'',
+});//substituindo um useState para cada input
+
+
+// Umas solução maior para os inputs
+/*     const handleNome = (e) => {setData({...data, nome: e.target.value})};
+    const handleEmail = (e) => {setData({...data, email: e.target.value})};
+    const handleSenha = (e) => {setData({...data, senha: e.target.value})}; */
+
+    // pegando dados de forma dinamica.
+    const handleInput = (e) =>{
+        const {value, name} = e.target;// propriedades que tem no inspesionar do navegador.
+        setData ({...data, [name]: value});
+    }
+
+    const handleSubmit =(e) =>{
+        e.preventDefault();// para nao dar reload na tela
+        alert(`Ola ${data.nome}, seu email é ${data.email}`)
+    }
+    return (
+        <>
+<form onSubmit={handleSubmit}>
+    {/*  trazendo const */}
+    <div>
+        <label htmlFor="nome">Nome</label>
+        <input type="text" name="nome" id="nome" onChange={handleInput} required/>
+    </div>
+    <div>
+        <label htmlFor="email">E-mail</label>
+        <input type="email" name="email" id="email" onChange={handleInput} required/>
+    </div>
+    <div>
+        <label htmlFor="senha">Senha</label>
+        <input type="password" name="senha" id="senha" onChange={handleInput} required/>
+    </div>
+    <div>
+        <button type='submit'>Enviar</button>
+    </div>
+</form>
+        </>
+    )
+}
+
+export default FormComponent
