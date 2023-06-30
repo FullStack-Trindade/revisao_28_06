3eed9b5bd171485782477a4150eacc915dbce411
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-06-30 17:49:05 -0300

    add show formComponent no app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import './App.css';
+import { useState } from 'react';
 import FormComponent from './components/FormComponent/FormComponent';
 import HeaderComponent from './components/HeaderComponent/HeaderComponent';
 import { UsuarioProvider } from './contexts/UsuarioContext';
 
 function App() {
+const [show, setShow] = useState(false);
+
+const showFormulario = () => {
+  setShow(!show);
+}
   return (
     <>
       <UsuarioProvider>
         <HeaderComponent/>
-        <FormComponent/>
+        { show?  <FormComponent/> : '' }
+        <button onClick={showFormulario}>{!show ? 'Exibir' : 'Ocultar'}</button>
       </UsuarioProvider>
     </>
 
